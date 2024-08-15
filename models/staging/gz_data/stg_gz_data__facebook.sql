b619e1f1cedce3c3a71a9bcc8cedea45fd61e271
patricia carolina riveros <carolina389riveros@gmail.com>
2024-08-15 23:57:36 +0000

    limpieza de tablas

--- models/staging/gz_data/stg_gz_data__facebook.sql
+++ models/staging/gz_data/stg_gz_data__facebook.sql
@@ -11,9 +11,9 @@ renamed as (
     select
         date_date,
         paid_source,
-        campaign_key,
-        campgn_name,
-        ads_cost,
+        campaign_key ,
+        campgn_name as campaign_name,
+        cast (ads_cost as float64) as ads_cost,
         impression,
         click
 
